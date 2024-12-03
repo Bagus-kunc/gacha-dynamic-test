e652cfdbeccdb33099688a660d5441d0d5b4b4f0
Bagus-kunc <bagus.kuncoro@talenavi.com>
2024-12-03 11:28:54 +0700

    fix: [BK] splashScreen

--- components/SplashScreen.vue
+++ components/SplashScreen.vue
@@ -94,7 +94,7 @@ const checkCaches = () => {
 <template>
   <div
     v-if="loading"
-    class="w-full max-w-md mx-auto h-screen overflow-hidden bg-[url('~/assets/images/bg-blue-green.png')] bg-cover bg-center flex flex-col fixed z-[1000]"
+    class="w-full max-w-md mx-auto h-screen overflow-hidden bg-[url('~/assets/images/bg-blue-green.png')] bg-cover bg-center flex flex-col fixed z-[2000]"
   >
     <div
       class="h-full w-full flex flex-col justify-center items-center text-exd-red"
--- pages/spin/[randomCode].vue
+++ pages/spin/[randomCode].vue
@@ -74,15 +74,6 @@
       <div
         class="w-full flex flex-col justify-center items-center gap-1.5 py-6 px-6 overflow-hidden relative"
       >
-        <!-- <img
-          :src="close"
-          alt="close"
-          width="30"
-          height="30"
-          preload
-          class="absolute right-2 top-3 cursor-pointer z-50"
-          @click="() => closeStepAllowLocation()"
-        /> -->
         <h3
           class="text-center max-w-[14rem] text-lg font-medium mt-2 flex-none"
         >
