1c6d4523a0ac3d099658b436f61fb8527efbe6c8
Bagus-kunc <bagus.kuncoro@talenavi.com>
2024-12-03 18:54:51 +0700

    fix: [BK] icon size history detail

--- pages/history/[id].vue
+++ pages/history/[id].vue
@@ -42,8 +42,7 @@
             v-else
             class="font-bold text-exd-1824.52 text-white p-1 min-h-10 min-w-12 h-10 w-12 flex items-center justify-center rounded-full pr-2 bg-no-repeat bg-contain bg-center"
             :style="rarityImg ? { backgroundImage: `url(${rarityImg})` } : {}"
-          >
-          </p>
+          ></p>
         </div>
         <div v-if="isFetching" class="flex items-center gap-5 text-exd-1218">
           <Skeleton
@@ -106,7 +105,7 @@
 
         <div
           v-if="socialMediaLinks.length"
-          class="inline-flex gap-3 w-full justify-center items-center mb-6 bg-exd-zinc-100 p-5 rounded-lg"
+          class="inline-flex md:gap-3 gap-2 w-full justify-center items-center mb-6 bg-exd-zinc-100 p-5 rounded-lg"
         >
           <img
             v-for="(link, index) in socialMediaLinks"
@@ -114,7 +113,7 @@
             :src="link.src"
             :alt="link.alt"
             :aria-label="link.alt"
-            class="size-7 cursor-pointer"
+            class="md:size-7 size-6 cursor-pointer"
             @click="openLink(link.url)"
             preload
           />
@@ -250,11 +249,11 @@ const star3 = ref(0)
 const colorBg = ref('')
 const rarityImg = ref('')
 
-const socialMediaLinks = ref([]);
+const socialMediaLinks = ref([])
 
 const openLink = (url) => {
-  window.open(url, '_blank');
-};
+  window.open(url, '_blank')
+}
 
 const loadGoogleMaps = () => {
   return new Promise((resolve, reject) => {
@@ -311,15 +310,15 @@ const fetchingHistoryData = async () => {
     star3Name.value = data.character_star_name3
 
     socialMediaLinks.value = [
-  { url: data.character.web1_link, src: web1, alt: 'Web 1' },
-  { url: data.character.web2_link, src: web2, alt: 'Web 2' },
-  { url: data.character.web3_link, src: web2, alt: 'Web 3' },
-  { url: data.character.line_link, src: line, alt: 'Line' },
-  { url: data.character.x_link, src: x, alt: 'X (Twitter)' },
-  { url: data.character.fb_link, src: facebook, alt: 'Facebook' },
-  { url: data.character.ig_link, src: instagram, alt: 'Instagram' },
-  { url: data.character.tt_link, src: tiktok, alt: 'TikTok' },
-].filter(link => link.url);
+      { url: data.character.web1_link, src: web1, alt: 'Web 1' },
+      { url: data.character.web2_link, src: web2, alt: 'Web 2' },
+      { url: data.character.web3_link, src: web2, alt: 'Web 3' },
+      { url: data.character.line_link, src: line, alt: 'Line' },
+      { url: data.character.x_link, src: x, alt: 'X (Twitter)' },
+      { url: data.character.fb_link, src: facebook, alt: 'Facebook' },
+      { url: data.character.ig_link, src: instagram, alt: 'Instagram' },
+      { url: data.character.tt_link, src: tiktok, alt: 'TikTok' },
+    ].filter((link) => link.url)
 
     // Memuat peta jika ada koordinat
     if (data.lat && data.long) {
