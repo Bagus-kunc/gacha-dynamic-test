ade74270cba15df5bcda8941ebbc0cda8420b08e
dewi-rect <52561113+dewi-rect@users.noreply.github.com>
2024-12-03 08:55:33 +0700

    fix: [SD] change manual frame

--- pages/manual/index.vue
+++ pages/manual/index.vue
@@ -1,3 +1,3 @@
 <template>
-    <iframe id="notion" style="height: 100vh;" src="https://endojidelakuji.super.site/" width="100%" height="1000" frameborder="0"></iframe>
+    <iframe id="notion" style="height: 100vh;" src="https://endo-ji-shotengai-dela-kuji.notaku.site/" width="100%" height="1000" frameborder="0"></iframe>
 </template>
