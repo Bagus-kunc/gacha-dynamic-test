96a9d9f3ebbcd1c449511b560fc5fc2c08f227d7
bayuaji676 <bayuaji676@gmail.com>
2024-12-05 20:08:51 +0700

    fix: [BA] link help & customer

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -131,7 +131,6 @@
         />
       </div>
     </div>
-
   </div>
 
   <Dialog
@@ -191,7 +190,7 @@ const handleGoToHistory = () => router.push('/history')
 const handleGoToPrize = () => router.push('/prize')
 const profile = () => router.push('/profile')
 const handleGoToHelp = () => {
-  window.open('https://endojishotengai.com/manual', '_blank')
+  window.open('https://endo-ji-shotengai.dela-kuji.jp/manual', '_blank')
 }
 const handleGoToDigitalMap = () => {
   window.open('https://nospot.new-ordinary.co.jp/maps/nagoya', '_blank')
