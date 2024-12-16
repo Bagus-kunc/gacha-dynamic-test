2e22aff51fbb6cbbffe02d4ffb3c94aaa5c1a2a3
bayuaji676 <bayuaji676@gmail.com>
2024-12-16 18:23:28 +0700

    fix: [BA] env timezone

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -181,6 +181,8 @@ import 'swiper/css/navigation'
 
 const router = useRouter()
 
+const config = useRuntimeConfig()
+
 definePageMeta({
   middleware: 'auth',
   layout: 'with-bottom-bar',
@@ -246,7 +248,7 @@ const checkSpinEligibility = async () => {
   const isAlreadySpin = sessionStorage.getItem('IS_ALREADY_SPIN')
   const spinType = sessionStorage.getItem('SPIN_TYPE')
   const readySpinAfterDate = sessionStorage.getItem('READY_SPIN_AFTER_DATE')
-  const now = new Date().toLocaleString('en-US', { timeZone: 'Asia/Tokyo' })
+  const now = formatDate(new Date())
 
   if (isAlreadySpin == 'true' && spinType === '1') {
     const message =
@@ -273,7 +275,7 @@ const checkSpinEligibility = async () => {
 
 function countdown(targetDate) {
   const remainingTime = () => {
-    const now = new Date().toLocaleString('en-US', { timeZone: 'Asia/Tokyo' })
+    const now = formatDate(new Date())
     const difference = new Date(targetDate).getTime() - new Date(now).getTime()
 
     if (difference <= 0) {
@@ -295,6 +297,21 @@ function countdown(targetDate) {
   isNotAllowed.value = true
 
   let intervals = setInterval(() => remainingTime(), 1000)
+}
+
+const formatDate = (date) => {
+  return new Date(date)
+    .toLocaleString('en-CA', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false,
+      timeZone: config.public.TIME_ZONE,
+    })
+    .replace(',', '')
 }
 
 onMounted(() => {
