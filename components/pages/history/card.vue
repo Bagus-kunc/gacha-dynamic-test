eb152703b2e34035f8db194c73c7a3b12c965763
Bagus-kunc <bagus.kuncoro@talenavi.com>
2025-06-04 16:39:39 +0700

    fix: [BK] history list

--- components/pages/history/card.vue
+++ components/pages/history/card.vue
@@ -8,7 +8,7 @@
     <template v-slot:text v-if="!isFetching">
       <div class="flex flex-col justify-center gap-1 pr-4 overflow-hidden">
         <p
-          class="font-bold text-exd-1824.52 text-white p-1 min-h-10 min-w-12 h-10 w-12 flex items-center justify-center rounded-full pr-2 bg-no-repeat bg-contain bg-center"
+          class="h-5 bg-no-repeat bg-contain bg-start"
           :style="raritySrc ? { backgroundImage: `url(${raritySrc})` } : {}"
         >
           <!-- {{ data.subtitle }} -->
