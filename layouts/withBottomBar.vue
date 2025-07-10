ca57d5c035e96ca18fa0d97a4032b389dfa257dd
Bagus-kunc <bagus.kuncoro@talenavi.com>
2025-07-10 09:06:57 +0700

    feat: [BK] integration ogp

--- layouts/withBottomBar.vue
+++ layouts/withBottomBar.vue
@@ -35,20 +35,20 @@ const { t } = useI18n()
 const settings = useState('settings')
 
 useHead({
-  title: t('headTitle'),
+  title: settings.value?.global?.ogp?.title,
   meta: [
-    { name: 'description', content: config.public.META_TITLE },
+    { name: 'description', content: settings.value?.global?.ogp?.title },
     // Facebook
-    { name: 'og:title', content: config.public.META_TITLE },
-    { name: 'og:description', content: config.public.META_DESCRIPTION },
+    { name: 'og:title', content: settings.value?.global?.ogp?.title },
+    { name: 'og:description', content: settings.value?.global?.ogp?.description },
     { name: 'og:image', content: config.public.META_IMAGE },
     { name: 'og:url', content: config.public.META_URL },
     { name: 'og:type', content: 'Website' },
     { name: 'og:image:width', content: '1200' },
     { name: 'og:image:height', content: '630' },
     // twitter
-    { name: 'twitter:title', content: config.public.META_TITLE },
-    { name: 'twitter:description', content: config.public.META_DESCRIPTION },
+    { name: 'twitter:title', content: settings.value?.global?.ogp?.title },
+    { name: 'twitter:description', content: settings.value?.global?.ogp?.description },
     { name: 'twitter:image', content: config.public.META_IMAGE },
     { name: 'twitter:card', content: 'summary_large_image' },
     { name: 'twitter:image:width', content: '1200' },
