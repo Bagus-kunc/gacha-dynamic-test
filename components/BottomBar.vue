9ed2b5e30b89c4abc9d8fa8ab83779581ed621b7
Bagus-kunc <bagus.kuncoro@talenavi.com>
2025-07-10 13:47:34 +0700

    feat: [BK] global_header_footer_color

--- components/BottomBar.vue
+++ components/BottomBar.vue
@@ -1,6 +1,9 @@
 <template>
   <div
-    class="fixed bottom-0 left-0 right-0 z-50 w-full h-16 max-w-md mx-auto bg-white"
+    class="fixed bottom-0 left-0 right-0 z-50 w-full h-16 max-w-md mx-auto"
+    :style="{
+      background: settings?.global?.global_header_footer_color
+    }"
   >
     <div class="inline-flex w-full gap-3">
       <div
@@ -78,8 +81,6 @@ const handleItems = () => {
     const isExternal = item.link_type === 'external';
     const label = item.footer_title_name?.value || '';
     const key = item.footer_title_name?.key;
-
-    console.log(item)
 
     const onClick = () => {
       if (isExternal) {
