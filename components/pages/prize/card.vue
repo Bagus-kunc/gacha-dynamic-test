06442295a9712c1d20bb32ada7c25f1b5324eb8d
Bagus-kunc <bagus.kuncoro@talenavi.com>
2025-07-16 14:05:13 +0700

    fix: [BK] text color date prize-lists

--- components/pages/prize/card.vue
+++ components/pages/prize/card.vue
@@ -25,7 +25,10 @@
                 {{ item.name }}
               </p>
               <p
-                class="text-exd-red-500 text-[10px] sm:text-[12px] font-medium"
+                class="text-[10px] sm:text-[12px] font-medium"
+                :style="{
+                  color: settings?.global?.text_colors?.secondary
+                }"
               >
                 {{ $t('applicationPeriod') }}：{{ item.started_at }}〜{{
                   item.expired_at
@@ -71,6 +74,7 @@ const props = defineProps({
 const totalGift = ref(null)
 
 const router = useRouter()
+const settings = useState('settings')
 const handleGoToDetailRedeem = (id) => router.push(`/prize/${id}`)
 
 const formatDate = (datetime) => {
